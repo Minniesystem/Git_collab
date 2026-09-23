5a3679ad6d44670eab3a94ecfa8d1b612eae78f8
69011151 <69011151@kmitl.ac.th>
2026-09-23 13:58:27 +0700

    edit function

--- Hello.c
+++ Hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-printf("Hello, Git Collaboration!\n");
-return 0;
+    greeting("Developer A,B,C !!");
+    return 0;
 }
+int greeting(const char *name) 
+{ 
+    printf("Hello, %s!\n", name); 
+    return 0; 
+} 
