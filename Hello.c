cb9fe93f3659d176415417e82321043539f7674b
69011151 <69011151@kmitl.ac.th>
2026-09-23 14:22:34 +0700

    comment

--- Hello.c
+++ Hello.c
@@ -4,6 +4,7 @@ int main(void)
     greeting("Developer A,B,C !!");
     return 0;
 }
+ // Print a greeting message for the specified user 
 int greeting(const char *name) 
 { 
     printf("Hello, %s!\n", name); 
