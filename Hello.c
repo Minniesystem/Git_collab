d44d576eb6ec764257fe6a55e0bce1e144d21fb9
69011001 <69011001@kmitl.ac.th>
2026-09-23 14:10:37 +0700

    Update greeting message in Hello.c

--- Hello.c
+++ Hello.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-    greeting("Developer A,B,C !!");
-    return 0;
+printf("Hello, Git Collaboration!\n");
+return 0;
 }
-int greeting(const char *name) 
-{ 
-    printf("Hello, %s!\n", name); 
-    return 0; 
-} 
